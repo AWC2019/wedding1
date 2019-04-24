703a16705a3d83cde52702844dae2c7bf38863f0
Lehlohonolo Mphuthi <lg.mphuhi1@gmail.com>
2019-04-24 10:26:30 +0200

    Updates

---
+++ scripts/app.js
@@ -0,0 +1,31 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set the initial state of the menu
+let showMenu = false;
+
+// Create a trigger event
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        //Set menu state
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        //Set menu state
+        showMenu = false;
+    }
+
+};
